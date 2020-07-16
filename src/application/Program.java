4ff14cb0bee579226237badf3312c31505802eb0
rafa-sant <rafaelb.santana99@gmail.com>
2020-07-15 22:04:36 -0300

    Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardaame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		Position pos = new Position(1, 2);
+		System.out.println(pos);
 	}
 
 }
